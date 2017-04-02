518dc41cb788b1c12d30f1695c293a8b1178208d
Damien DUPORTAL <damien.duportal@gmail.com>
2017-04-02 09:53:43 +0200

    Jenkins: auto add the shared pipeline library
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

---
+++ docker/jenkins/ref/init.groovy.d/addSharedLibrary.groovy
@@ -0,0 +1,30 @@
+import jenkins.model.*;
+
+import org.jenkinsci.plugins.workflow.libs.*;
+import jenkins.scm.api.SCMSource;
+
+GlobalLibraries globalLibs = GlobalConfiguration.all().get(GlobalLibraries.class)
+
+SCMSource scm = new jenkins.plugins.git.GitSCMSource(
+  "",
+  "https://github.com/oufti-playground/example-voting-app",
+  "",
+  "",
+  "",
+  false
+)
+
+LibraryRetriever libRetriever = new SCMSourceRetriever(scm)
+
+LibraryConfiguration libConfig = new LibraryConfiguration(
+  "deploy",
+  libRetriever
+)
+
+libConfig.setDefaultVersion("master")
+libConfig.setImplicit(true)
+
+List<LibraryConfiguration> libraries= new ArrayList<LibraryConfiguration>()
+libraries.add(libConfig)
+
+globalLibs.setLibraries(libraries)
