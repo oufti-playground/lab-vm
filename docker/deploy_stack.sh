975e9e951dc0f652a89397387e80fdfdb2574d91
Damien DUPORTAL <damien.duportal@gmail.com>
2017-06-07 10:31:51 +0200

    Adding Slides inside the docker-compose stack, for both local VM and Swarm mode, with subs replace for hostnames
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- docker/deploy_stack.sh
+++ docker/deploy_stack.sh
@@ -5,8 +5,9 @@ set -eux -o pipefail
 . .load_env.sh
 
 COMPOSE_PROJECT_NAME="${1}"
+COMPOSE_FILE="${2:-docker-compose.yml}"
 EXTERNAL_DOMAIN="${COMPOSE_PROJECT_NAME}.${BASE_DOMAIN}"
 
 export COMPOSE_PROJECT_NAME EXTERNAL_DOMAIN
 
-docker stack deploy --compose-file docker-compose.yml "${COMPOSE_PROJECT_NAME}"
+docker stack deploy --compose-file "${COMPOSE_FILE}" "${COMPOSE_PROJECT_NAME}"
