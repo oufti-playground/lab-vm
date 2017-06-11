a0c2da60fd855c612f844cb86f33db4d588a5964
Damien DUPORTAL <damien.duportal@gmail.com>
2017-06-11 19:50:49 +0200

    Swarm scripts fix
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- swarm-scripts/load_env.sh
+++ swarm-scripts/load_env.sh
@@ -1,9 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eux -o pipefail
 
+# source ../docker/.env
+# while read -r line; do declare  $line; done <../docker/.env
+read_properties()
+{
+  file="$1"
+  while IFS="=" read -r key value; do
+    case "$key" in
+      '#'*) ;;
+      *)
+        eval "$key=\"$value\""
+    esac
+  done < "$file"
+}
+
 BASE_DOMAIN="cloudbees-training.com"
-. ../docker/.env
+read_properties "../docker/.env"
 REGISTRY_URL="${BASE_DOMAIN}:5000"
 
 
