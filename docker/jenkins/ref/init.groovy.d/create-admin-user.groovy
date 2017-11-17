9f12ea693339d5f6c6c1fa80875e924d638ebd98
Damien DUPORTAL <damien.duportal@gmail.com>
2017-11-17 20:23:29 +0100

    Jenkins: better caching in Dockerfile + FIXING THAT DAMN EMAIL I HATE GROOVY
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- docker/jenkins/ref/init.groovy.d/create-admin-user.groovy
+++ docker/jenkins/ref/init.groovy.d/create-admin-user.groovy
@@ -1,21 +1,22 @@
 import jenkins.model.*
 import hudson.security.*
+import hudson.model.*
 
 def username = 'butler'
-def password = "${username}"
-def user_email = "${username}@localhost.local"
+def password = username
+def userEmail = "${username}@localhost.local"
 
 def instance = Jenkins.getInstance()
 
 // Define default admin user
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("${username}","${password}")
+hudsonRealm.createAccount(username,password)
 instance.setSecurityRealm(hudsonRealm)
 instance.save()
 
 // Set User email
-def user = User.get("${username}")
+def user = User.get(username)
 
-def email_param = new hudson.tasks.Mailer.UserProperty("${user_email}")
+def email_param = new hudson.tasks.Mailer.UserProperty(userEmail)
 user.addProperty(email_param)
 user.save()
