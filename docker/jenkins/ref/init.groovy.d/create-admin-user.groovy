6cdebe22e373b9c6d466380ca1d04979183061b9
Damien DUPORTAL <damien.duportal@gmail.com>
2017-11-17 20:17:41 +0100

    Jenkins: auto-config the user default email to avoid BlueOcean 500 errors
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- docker/jenkins/ref/init.groovy.d/create-admin-user.groovy
+++ docker/jenkins/ref/init.groovy.d/create-admin-user.groovy
@@ -1,9 +1,21 @@
 import jenkins.model.*
 import hudson.security.*
 
+def username = 'butler'
+def password = "${username}"
+def user_email = "${username}@localhost.local"
+
 def instance = Jenkins.getInstance()
 
+// Define default admin user
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount("butler","butler")
+hudsonRealm.createAccount("${username}","${password}")
 instance.setSecurityRealm(hudsonRealm)
 instance.save()
+
+// Set User email
+def user = User.get("${username}")
+
+def email_param = new hudson.tasks.Mailer.UserProperty("${user_email}")
+user.addProperty(email_param)
+user.save()
