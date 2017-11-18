63036cf5a04323fb43f3a9e283f8fc152c761681
Damien DUPORTAL <damien.duportal@gmail.com>
2017-11-18 09:19:10 +0100

    Devbox:  upgrade to the base dind java image to have its own docker engine and no more compelxity with supervisor
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

---
+++ docker/devbox/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -ex
+
+# Lauch Docker Engine using parent image's entrypoint script
+bash /usr/local/bin/dind-entrypoint.sh >/dev/stdout 2>&1 &
+
+# Set default user git env
+su -c "git config --global user.email ${DEFAULT_USER}@localhost.local" \
+  "${DEFAULT_USER}"
+su -c "git config --global user.name ${DEFAULT_USER}" "${DEFAULT_USER}"
+
+# Custom CLI
+echo 'export PS1="\u@devbox [\w]> "' \
+  | tee -a /home/${DEFAULT_USER}/.bashrc
+
+# Add some entropy
+rngd -r /dev/urandom
+
+# Launch TTYD server in foreground
+exec su -c "ttyd -I /index.html bash" "${DEFAULT_USER}"
