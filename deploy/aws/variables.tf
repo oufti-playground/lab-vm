783992f0ffb56456c42c5e4ba415d89f0e9b3d77
Damien DUPORTAL <damien.duportal@gmail.com>
2017-11-30 12:09:41 +0100

    Adding Franfurt as default
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- deploy/aws/variables.tf
+++ deploy/aws/variables.tf
@@ -2,7 +2,8 @@ variable "lab_amis" {
   description = "Mapping regions to the lab AMI IDs"
   type = "map"
   default = {
-    "us-east-1"="ami-6151cf1b"
+    "us-east-1" = "ami-6151cf1b"
+    "eu-central-1" = "ami-f9ee6196"
   }
 }
 
@@ -14,5 +15,5 @@ variable "lab_instances_count" {
 variable "lab_region" {
   description = "AWS Region where to launch instances"
   type = "string"
-  default = "us-east-1"
+  default = "eu-central-1"
 }
