7a0e0e87a8a443ea2fd0219df551e7b0de3c822a
Damien DUPORTAL <damien.duportal@gmail.com>
2018-05-27 20:46:30 +0200

    Gitserver: latest version 1.4.x + auto-configuring Blue Ocean SSH key + new loading system + webhook autoconfigured
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- docker/gitserver/entrypoint.sh
+++ docker/gitserver/entrypoint.sh
@@ -3,10 +3,30 @@
 # Custom Entrypoint for Gite that will render templates before starting
 #
 
+set -ex -o pipefail
+
+# Check if we need custom configuration (can be done at runtime instead of build time)
+set +u
+if [ ! -f /app/data/.semaphore ] && [ -n "${SOURCE_REPO_TO_MIRROR}" ] && [ -n "${FIRST_USER}" ]
+then
+  echo "== No Semaphore file found in /app/data/.semaphore"
+  echo "=> Conditions found for runtime customization. Launching in background"
+
+  # Run customization in background
+  nohup bash /app/setup-gitea.sh >/dev/stdout 2>/dev/stdout &
+fi
+set -u
+
 cat "${SERVICE_CONFIG_FILE}.tmpl" \
   | sed "s#ROOT_URL.*=.*#ROOT_URL = ${EXTERNAL_URL}#g" \
   | sed "s#^DOMAIN.*#DOMAIN = ${EXTERNAL_DOMAIN}#g" \
   | tee /data/gitea/conf/app.ini
+
+ # Run the SSH key loader in background if requested
+if [ "${LOAD_SSH_KEY_FROM_JENKINS}" = "true" ]
+then
+  nohup bash /usr/local/bin/set-key-from-jenkins-to-gitea.sh >/dev/stdout 2>/dev/stdout &
+fi
 
 # Standard entrypoint
 exec /usr/bin/entrypoint
--- docker/gitserver/setup-gitea.sh
+++ docker/gitserver/setup-gitea.sh
@@ -1,28 +1,76 @@
 #!/bin/bash
 #
-
-set -e -o pipefail -x
+set -ex -o pipefail
 
 # Parameters
-GITSERVER_API_URL="${EXTERNAL_URL}/api/v1"
+GITSERVER_BASE_URL="${EXTERNAL_URL:-http://localhost:3000}"
+GITSERVER_API_URL="${GITSERVER_BASE_URL}/api/v1"
 GITEA_RUN_USER=$(awk -F "=" '/RUN_USER/ {print $2}' ${SERVICE_CONFIG_FILE}* | tr -d ' ' | head -n1)
 CUSTOM_DATA_DIRECTORY="/app/data"
+WEBHOOKS_BASE_URL="http://jenkins:8080/jenkins/git/notifyCommit?url=ssh://git@gitserver:5022"
 
-# Initial checks
-id -u ${GITEA_RUN_USER} || (echo "${GITEA_RUN_USER} user does not exists" && exit 1)
-mkdir -p "${CUSTOM_DATA_DIRECTORY}"
-chown -R "${GITEA_RUN_USER}" "${CUSTOM_DATA_DIRECTORY}"
+# Reusable functions
 
-# We need a prebaked configuration
-cp "${SERVICE_CONFIG_FILE}.tmpl" "${SERVICE_CONFIG_FILE}"
+mirror_repository_into_local_gitserver() {
 
-# Launching the Gitserver
-/usr/bin/entrypoint >/dev/stdout 2>&1 &
+  REPO_NAME="${1}"
+  shift
+
+  REMOTE_REPO_URL="${1}"
+  shift
+
+  IS_PRIVATE="${1}"
+  shift
+
+  WEBHOOK="${1}"
+  shift
+
+  echo "==== Cloning local repository /tmp/${REPO_NAME} from ${REMOTE_REPO_URL}"
+  git clone --bare "${REMOTE_REPO_URL}" "/tmp/${REPO_NAME}"
+
+  echo "==== Creating repository ${REPO_NAME} inside Gitserver"
+  curl -v --fail -X POST -s \
+    -F "name=${REPO_NAME}" \
+    -F "private=${IS_PRIVATE}" \
+    -u "${FIRST_USER}:${FIRST_USER}" \
+    "${GITSERVER_API_URL}/user/repos"
+  REPO_URL="$(echo ${GITSERVER_BASE_URL} | sed "s#://#://${FIRST_USER}:${FIRST_USER}@#")/${FIRST_USER}/${REPO_NAME}"
+
+  echo "==== Mirroring local repository /tmp/${REPO_NAME} to ${REPO_URL} in Gitserver"
+  cd "/tmp/${REPO_NAME}" && git push --mirror "${REPO_URL}" && cd -
+
+  if [ -n "${WEBHOOK}" ] && [ "${WEBHOOK}" == "true" ]
+  then
+    echo "==== Adding a Webhook with this payload URL: ${WEBHOOK}"
+
+    WEBHOOK_URL="${WEBHOOKS_BASE_URL}/${FIRST_USER}/${REPO_NAME}.git"
+
+    curl --fail -v -X POST -s \
+      -u "${FIRST_USER}:${FIRST_USER}" \
+      -H "accept: application/json" -H "Content-Type: application/json" \
+      -d "{ \"active\": true, \"config\": { \"url\": \"${WEBHOOK_URL}\", \"content_type\": \"json\" }, \"events\": [ \"push\",\"create\", \"pull_request\",\"repository\"], \"type\": \"gitea\"}" \
+      "${GITSERVER_API_URL}/repos/${FIRST_USER}/${REPO_NAME}/hooks"
+  fi
+}
+
+if [ "${1}" == "start-gitserver" ]
+then
+  # Initial checks
+  id -u ${GITEA_RUN_USER} || (echo "${GITEA_RUN_USER} user does not exists" && exit 1)
+  mkdir -p "${CUSTOM_DATA_DIRECTORY}"
+  chown -R "${GITEA_RUN_USER}" "${CUSTOM_DATA_DIRECTORY}"
+
+  # We need a prebaked configuration
+  cp "${SERVICE_CONFIG_FILE}.tmpl" "${SERVICE_CONFIG_FILE}"
+
+  # Launching the Gitserver
+  nohup /usr/bin/entrypoint >/dev/stdout 2>/dev/stdout &
+fi
 
 # Waiting for the Gitserver to start
 TIME_TO_WAIT=2
 while true; do
-  curl --fail -s -v -X GET "${EXTERNAL_URL}" \
+  curl -sSL -o /dev/null --fail "${GITSERVER_BASE_URL}" \
   && break ||
     echo "Gitserver still not started, waiting ${TIME_TO_WAIT}s before retrying"
 
@@ -33,19 +81,19 @@ echo "== Configuring Git Server"
 
 # We create the first user
 echo "=== Creating initial user as ${FIRST_USER}"
-curl -v -X POST -s \
+curl --fail -v -X POST -s \
   -F "user_name=${FIRST_USER}" \
   -F "email=${FIRST_USER}@localhost.local" \
   -F "password=${FIRST_USER}" \
   -F "retype=${FIRST_USER}" \
-  ${EXTERNAL_URL}/user/sign_up
+  ${GITSERVER_BASE_URL}/user/sign_up
 
 if [ -n "${SOURCE_REPO_TO_MIRROR}" ]
 then
   echo "=== Loading the custom repos"
   OLD_IFS=$IFS
   # The ';' char is allowed in URLs, but I assume the risk of having it
-  # for git repos is low. If you find this case: find me, shout me :)
+  # for git repos is low. If you find this case: find me, shoot me :)
   IFS=';'
 
   #Use /tmp to fetch a SSH key
@@ -69,23 +117,18 @@ then
     else
       REPO_PRIVATE=false
     fi
+    if [ -n "$(echo ${REPO} | cut -f4 -d'|')" ]
+    then
+      WEBHOOK=true
+    else
+      WEBHOOK=false
+    fi
 
-
-    echo "==== Cloning local repository /tmp/${REPO_NAME} from ${REMOTE_REPO_URL}"
-    git clone --bare ${REMOTE_REPO_URL} /tmp/${REPO_NAME}
-
-
-    echo "==== Creating repository ${REPO_NAME} inside Gitserver"
-    curl -v -X POST -s \
-      -F "name=${REPO_NAME}" \
-      -F "private=${REPO_PRIVATE}" \
-      -u "${FIRST_USER}:${FIRST_USER}" \
-      ${GITSERVER_API_URL}/user/repos
-    REPO_URL="$(echo ${EXTERNAL_URL} | sed "s#://#://${FIRST_USER}:${FIRST_USER}@#")/${FIRST_USER}/${REPO_NAME}"
-
-    echo "==== Mirroring local repository /tmp/${REPO_NAME} to ${REPO_URL} in Gitserver"
-
-    cd /tmp/${REPO_NAME} && git push --mirror ${REPO_URL} && cd -
+    mirror_repository_into_local_gitserver \
+      "${REPO_NAME}" \
+      "${REMOTE_REPO_URL}" \
+      "${REPO_PRIVATE}" \
+      "${WEBHOOK}"
 
   done
   rm -rf /tmp/* ~/.ssh/id_rsa
@@ -94,4 +137,5 @@ else
   echo "== No repo to mirror nor preload"
 fi
 
+touch "${CUSTOM_DATA_DIRECTORY}/.semaphore"
 echo "== Configuration done."
